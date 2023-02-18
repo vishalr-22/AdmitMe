8f8bc1a0e4a0a686f1dc1ffeaa4472d8ba806cd6
Dheepa Ramanathan <vishalramanathan22@gmail.com>
2023-02-18 22:39:02 +0530

    backend initialised

---
+++ admitmebackend/src/main/java/com/example/admitmebackend/AdmitmebackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.admitmebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmitmebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmitmebackendApplication.class, args);
+	}
+
+}
